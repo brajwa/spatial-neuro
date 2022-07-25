21b03eeb4c043c863ce9eae5fa499c417efdf88b
Abida Sanjana Shemonti <ashemont@purdue.edu>
2022-07-24 21:15:26 -0400

    codes added

---
+++ quantifyVagusNerve/Codes/LfunctAnova2.R
@@ -0,0 +1,88 @@
+#### loading required libraries (there might be more libraries loaded than required)
+load.lib = c("deldir", "spatstat", "spatstat.utils", "magrittr", "dplyr", "igraph", "scales", "httr", "tidyverse", "ggnetwork", "ggplot2", "poweRlaw",
+             "imager", "viridis", "plotrix", "openxlsx", "tidyr", "spdep", "maptools", "tmap", "OpenImageR", "dismo", "lctools",
+             "officer", "rvg", "oce", "OneR", "RandomFieldsUtils", "RandomFields", "Cairo", "knitr", "scorepeak", "Rcpp", "emdist",
+             "RImageJROI", "svglite", "transport", "Barycenter", "T4transport", "OpenImageR", "wvtool", "adimpro", "reshape2",
+             "this.path")
+
+install.lib = load.lib[!load.lib %in% installed.packages()]
+for(lib in install.lib) install.packages(lib, dependencies=TRUE)
+sapply(load.lib, require, character=TRUE)
+
+
+#### extracting parent directory information for accessing input and output location
+dir = this.dir()
+folder = strsplit(dir, "/")
+folder = folder[[1]][length(folder[[1]])]
+parent = strsplit(dir, folder)
+
+
+#### input location
+folder_path = paste(parent, "Data/Inputs/", sep = "")
+data_files = list.files(path=folder_path, pattern = ".csv", full.names = FALSE)
+extension = ".csv"
+
+
+#### removing the sample with quite different L-function behavior for r-value computation
+#### sample: 11328_5_AB-2V_2_montage_pred
+data_files = data_files[data_files != "11328_5_AB-2V_2_montage_pred_mi.csv"]
+
+
+pp_list = list()
+
+### computing the max r values in inhomogeneous L function for each sample
+max_inhom_l_r = c()
+
+for (file_name in data_files) {
+  file_name = strsplit(file_name, ".csv")[[1]]
+  print(file_name)
+  
+  axon_locations = unique(read.csv(paste(folder_path, file_name, extension, sep="")))
+  retrieved_contour = readRDS(paste(folder_path, file_name, ".rds", sep=""))
+  
+  axon_pp = ppp(x=axon_locations$X, y=axon_locations$Y, checkdup=F, window = retrieved_contour)
+  
+  axon_pp = rescale.ppp(axon_pp, s=axon_pp$window$yrange[2])
+  axon_pp = shift.ppp(axon_pp, origin = "centroid")
+  
+  pp_list[[length(pp_list) + 1]] = axon_pp
+  
+  inhom_l = Linhom(axon_pp, correction = "border")
+  cat("Max r in Linhom: ", max(inhom_l$r), "\n\n")
+  
+  max_inhom_l_r[length(max_inhom_l_r) + 1] = max(inhom_l$r)
+}
+
+
+#### creating a common range of r-values
+r_vect_size = 100
+inhom_l_r_vect = seq(0, min(max_inhom_l_r), by=(min(max_inhom_l_r)/r_vect_size))
+
+
+inhom_l_all = data.frame(sample=data_files, group=c(rep(1, 17), rep(2, 11))) #1=vagus,2=pelvic
+
+
+#### computing inhomogeneous L function for the common range of r values (just computed above) for all the samples
+#### to figure out the most varying r. This r will be used in local computations.
+for(i in c(1:length(pp_list))){
+  axon_pp = pp_list[[i]]
+  inhom_l = Linhom(axon_pp, r=inhom_l_r_vect, correction = "border")
+  
+  inhom_l_all[i, 3:103] = inhom_l$border
+}
+
+
+f_val_vector = c()
+
+for(i in c(3:103)){
+  anova_res = summary(aov(inhom_l_all[, i]~inhom_l_all$group))
+  f_val_vector[i-2] = anova_res[[1]]$`F value`[1]
+}
+
+f_ratio = data.frame(r=inhom_l_r_vect, f_ratio=f_val_vector)
+f_ratio = f_ratio[order(f_ratio$f_ratio, decreasing = TRUE),]
+
+#### top 3 r values with top variances
+top_r = f_ratio[1:3,]$r
+
+top_r[1] # the r-value to be used
---
+++ quantifyVagusNerve/Codes/T4transportSpatialIntensity.R
@@ -0,0 +1,314 @@
+#### loading required libraries (there might be more libraries loaded than required)
+load.lib = c("deldir", "spatstat", "spatstat.utils", "magrittr", "dplyr", "igraph", "scales", "httr", "tidyverse", "ggnetwork", "ggplot2", "poweRlaw",
+             "imager", "viridis", "plotrix", "openxlsx", "tidyr", "spdep", "maptools", "tmap", "OpenImageR", "dismo", "lctools",
+             "officer", "rvg", "oce", "OneR", "RandomFieldsUtils", "RandomFields", "Cairo", "knitr", "scorepeak", "Rcpp", "emdist",
+             "RImageJROI", "svglite", "transport", "Barycenter", "T4transport", "OpenImageR", "wvtool", "adimpro",
+             "proxy", "RColorBrewer", "tictoc", "this.path")
+
+install.lib = load.lib[!load.lib %in% installed.packages()]
+for(lib in install.lib) install.packages(pkgs=lib, dependencies=TRUE)
+sapply(load.lib, require, character=TRUE)
+
+
+#### command line arguments contain analysis type, scaling information and entropy regularizer lambda
+args = commandArgs()
+print(args)
+
+#### the number of arguments to check for should be changed if required
+#### 10 arguments are used while using Rscript command from RStudio terminal
+#### 6 arguments are required if run with slurm job
+if(length(args) != 10){
+  print("Invalid arguments...\n")
+  return()
+} else{
+  analysis_type = args[7]
+  scaling = args[8]
+  lambda  = args[9]
+  version = args[10]
+}
+
+print(analysis_type)
+print(scaling)
+print(lambda)
+print(version)
+
+
+#### extracting parent directory information for accessing input and output location
+dir = this.dir()
+folder = strsplit(dir, "/")
+folder = folder[[1]][length(folder[[1]])]
+parent = strsplit(dir, folder)
+
+#### output plot location
+plot_dest_path = paste(parent, "Plots/T4transport/", version, "_", analysis_type, scaling, "_", lambda, "/", sep="")
+if (!dir.exists(plot_dest_path)) {dir.create(plot_dest_path, recursive=TRUE)}
+
+
+#### source the inhomogeneous sector functions
+source("LocalKSector.R")
+source("Kinhomsector.R")
+
+
+#### input location
+if(version == "complete"){
+  folder_path = paste(parent, "Data/Inputs/", sep = "")
+}else if(version == "demo"){
+  folder_path = paste(parent, "Data/Demo Inputs/", sep = "")
+}else{
+  print("Invalid version...\n")
+  return()
+}
+data_files = list.files(path=folder_path, pattern = ".csv", full.names = FALSE)
+extension = ".csv"
+
+
+#### point pattern and plots, comment out the while loop block if images are not required
+i = 1
+while (i <= length(data_files)) {
+  
+  #construct the i-th point pattern
+  file_name = strsplit(data_files[i], ".csv")[[1]]
+  print(file_name)
+  
+  axon_locations = unique(read.csv(paste(folder_path, file_name, extension, sep="")))
+  retrieved_contour = readRDS(paste(folder_path, file_name, ".rds", sep=""))
+  
+  axon_pp = ppp(x=axon_locations$X, y=axon_locations$Y, checkdup=F, window = retrieved_contour)
+  
+  if(scaling == 1){
+  	print("scaled")
+  	axon_pp = rescale.ppp(axon_pp, s=axon_pp$window$yrange[2]) 
+  }
+  axon_pp = shift.ppp(axon_pp, origin = "centroid")
+  
+  axon_pp = as.ppp(axon_pp)
+  svglite(paste(plot_dest_path, file_name, "_pp.svg", sep=""), width = 10, height = 10)
+  plot(axon_pp, pch=21, bg="black", cex=0.7, main="")
+  dev.off()
+  
+  if(analysis_type=="basic_density"){
+  	#basic density
+  	dens = density(axon_pp, sigma=bw.diggle(axon_pp), adjust = 2, diggle=TRUE, eps=0.0025)
+  	dens_mat = t(dens$v)
+  
+  	jpeg(paste(plot_dest_path, file_name,"_Density.jpeg", sep=""),
+  	   width = 10,
+  	   height = 10,
+  	   units = "in",
+  	   res=900)
+  	plot(dens, main="", col=colorRampPalette(brewer.pal(9, "YlOrRd"))(9), box=FALSE)
+  	dev.off()
+    
+  }else if(analysis_type=="local_linhom"){
+  	pp_l = localLinhom(axon_pp, rvalue = 0.0352, verbose=FALSE)
+    marks(axon_pp) = pp_l
+    dens = Smooth(axon_pp, sigma=bw.diggle(axon_pp), adjust=2, diggle=TRUE, eps=0.0025)
+  	
+  	jpeg(paste(plot_dest_path, file_name,"_Local_Linhom.jpeg", sep=""),
+  	   width = 10,
+  	   height = 10,
+  	   units = "in",
+  	   res=900)
+  	plot(dens, main="", col=colorRampPalette(brewer.pal(9, "YlOrRd"))(9), box=FALSE)
+  	dev.off()
+	
+  }else if(analysis_type=="local_linhom_sector_horizontal"){
+  	pp_l = localLinhomsector(axon_pp, rvalue = 0.0352, begin=-15, end=15, units="degrees", verbose=FALSE)
+    marks(axon_pp) = pp_l
+    dens = Smooth(axon_pp, sigma=bw.diggle(axon_pp), adjust=2, diggle=TRUE, eps=0.0025)
+  	
+  	jpeg(paste(plot_dest_path, file_name,"_Local_Linhom_H.jpeg", sep=""),
+  	   width = 10,
+  	   height = 10,
+  	   units = "in",
+  	   res=900)
+  	plot(dens, main="", col=colorRampPalette(brewer.pal(9, "YlOrRd"))(9), box=FALSE)
+  	dev.off()
+     
+  }else if(analysis_type=="local_linhom_sector_vertical"){
+    pp_l = localLinhomsector(axon_pp, rvalue = 0.0352, begin=90-15, end=90+15, units="degrees", verbose=FALSE)
+    marks(axon_pp) = pp_l
+    dens = Smooth(axon_pp, sigma=bw.diggle(axon_pp), adjust=2, diggle=TRUE, eps=0.0025)
+  	
+  	jpeg(paste(plot_dest_path, file_name,"_Local_Linhom_V.jpeg", sep=""),
+  	   width = 10,
+  	   height = 10,
+  	   units = "in",
+  	   res=900)
+  	plot(dens, main="", col=colorRampPalette(brewer.pal(9, "YlOrRd"))(9), box=FALSE)
+  	dev.off()
+  }else{
+    print("Invalid analysis type!")
+    return()
+  }
+  
+  i = i + 1
+}
+
+
+#### creating workbook to save results
+wb = openxlsx::createWorkbook()
+distance_matrix = matrix(0, nrow = length(data_files), ncol = length(data_files))
+min_angle_matrix = matrix(0, nrow = length(data_files), ncol = length(data_files))
+
+
+#### computing pair-wise Sinkhorn distance
+i = 1
+while (i <= length(data_files)) {
+  sinkhorn_info = data.frame(matrix(0, nrow = 8, ncol = length(data_files))) # 8 rows, one for each orientation, 
+                                                                              # because we are rotating by 45 degree
+
+  j = i+1
+  while (j <= length(data_files)) {
+    cat("Fascicle pair: ", i, " ", j, "\n")
+    
+    #construct the i-th point pattern
+    file_name_1 = strsplit(data_files[i], ".csv")[[1]]
+    print(file_name_1)
+    
+    axon_locations_1 = unique(read.csv(paste(folder_path, file_name_1, extension, sep="")))
+    retrieved_contour_1 = readRDS(paste(folder_path, file_name_1, ".rds", sep=""))
+    
+    axon_pp_1 = ppp(x=axon_locations_1$X, y=axon_locations_1$Y, checkdup=F, window = retrieved_contour_1)
+    
+    if(scaling == 1){
+    	print("scaled")
+    	axon_pp_1 = rescale.ppp(axon_pp_1, s=axon_pp_1$window$yrange[2])
+    }
+    axon_pp_1 = shift.ppp(axon_pp_1, origin = "centroid")
+    
+    axon_pp_1 = as.ppp(axon_pp_1)
+    
+    #construct the j-th point pattern
+    file_name_2 = strsplit(data_files[j], ".csv")[[1]]
+    print(file_name_2)
+    
+    axon_locations_2 = unique(read.csv(paste(folder_path, file_name_2, extension, sep="")))
+    retrieved_contour_2 = readRDS(paste(folder_path, file_name_2, ".rds", sep=""))
+    
+    axon_pp_2 = ppp(x=axon_locations_2$X, y=axon_locations_2$Y, checkdup=F, window = retrieved_contour_2)
+    
+    if(scaling == 1){
+    	print("scaled")
+    	axon_pp_2 = rescale.ppp(axon_pp_2, s=axon_pp_2$window$yrange[2])
+    }
+    axon_pp_2 = shift.ppp(axon_pp_2, origin = "centroid") 
+    
+    axon_pp_2 = as.ppp(axon_pp_2)
+    
+    col_info = c()
+    row_info = c()
+    
+    #loop for rotating the i-th point pattern
+    angle_of_rotation = 45 # in degree
+    theta = 0
+    while(theta < 360){
+      cat("theta: ", theta, "\n")
+      row_info[length(row_info) + 1] = theta
+      
+      axon_pp_1_rotated = rotate.ppp(axon_pp_1, angle = theta*pi/180, centre = "centroid")
+      svglite(paste(plot_dest_path, paste(file_name_1, theta, "deg rotated", sep = " "), "_pp.svg", sep=""), width = 10, height = 10)
+      plot(axon_pp_1_rotated, pch=21, bg="black", cex=0.7, main=paste(file_name_1, theta, "deg rotated", sep = " "))
+      dev.off()
+      
+      X = as.matrix(data.frame(axon_pp_1_rotated$x, axon_pp_1_rotated$y))
+      Y = as.matrix(data.frame(axon_pp_2$x, axon_pp_2$y))
+      
+      if(analysis_type=="basic_density"){
+    		WX = rep(1/axon_pp_1_rotated$n, axon_pp_1_rotated$n)
+    		WY = rep(1/axon_pp_2$n, axon_pp_2$n)
+		
+  	  }else if(analysis_type=="local_linhom"){
+    		pp1_l = localLinhom(axon_pp_1_rotated, rvalue = 0.0352, verbose=FALSE)
+    		pp2_l = localLinhom(axon_pp_2, rvalue = 0.0352, verbose=FALSE)
+    		
+    		WX = pp1_l / sum(pp1_l)
+    		WY = pp2_l / sum(pp2_l)
+  		
+  	  }else if(analysis_type=="local_linhom_sector_horizontal"){
+    		pp1_l = localLinhomsector(axon_pp_1_rotated, rvalue = 0.0352, begin=-15, end=15, units="degrees", verbose=FALSE)
+    		pp2_l = localLinhomsector(axon_pp_2, rvalue = 0.0352, begin=-15, end=15, units="degrees", verbose=FALSE)
+    		
+    		WX = pp1_l / sum(pp1_l)
+    		WY = pp2_l / sum(pp2_l)
+  		 
+  	  }else if(analysis_type=="local_linhom_sector_vertical"){
+    		pp1_l = localLinhomsector(axon_pp_1_rotated, rvalue = 0.0352, begin=90-15, end=90+15, units="degrees", verbose=FALSE)
+    		pp2_l = localLinhomsector(axon_pp_2, rvalue = 0.0352, begin=90-15, end=90+15, units="degrees", verbose=FALSE)
+    		
+    		WX = pp1_l / sum(pp1_l)
+    		WY = pp2_l / sum(pp2_l)
+  
+  	  }else{
+    		print("Invalid analysis type!")
+  	    return()
+  	  }
+	  
+  	  #cat("wx:\n", WX, "\nsum wx: ", sum(WX), "\n")
+  	  #cat("wy:\n", WY, "\nsum wy: ", sum(WY), "\n")
+      
+      #using the function from T4transport package
+  	  if(lambda == -1){
+  	  	print("Computing wasserstein distance...")
+  	  	tic("Wasserstein")
+    		dist_t4 = T4transport::wasserstein(X, Y)
+    		toc()
+    		cat(dist_t4$distance, "\n")
+  	  }
+  	  else{
+  	  	print("Computing sinkhorn distance...")
+  	  	tic(paste("Sinkhorn lambda = ", lambda, sep=""))
+  	  	dist_t4 = T4transport::sinkhorn(X, Y, lambda = lambda, wx=WX, wy=WY) #default regularization param (lambda) is 0.1.
+  	  	toc()
+  	  	cat(dist_t4$distance, "\n")
+  	  }
+        
+      col_info[length(col_info) + 1] = dist_t4$distance
+      theta = theta + angle_of_rotation
+    }
+    
+    sinkhorn_info[, j] = col_info
+    j = j+1
+  }
+  
+  #include min, max, avg, variance and min index for all the Sinkhorn distance per fascicle
+  sinkhorn_info[nrow(sinkhorn_info) + 1, ] = apply(sinkhorn_info[1:8, ], 2, min)
+  sinkhorn_info[nrow(sinkhorn_info) + 1, ] = apply(sinkhorn_info[1:8, ], 2, max)
+  sinkhorn_info[nrow(sinkhorn_info) + 1, ] = apply(sinkhorn_info[1:8, ], 2, mean)
+  sinkhorn_info[nrow(sinkhorn_info) + 1, ] = apply(sinkhorn_info[1:8, ], 2, var)
+  sinkhorn_info[nrow(sinkhorn_info) + 1, ] = row_info[apply(sinkhorn_info[1:8, ], 2, which.min)]
+  
+  rownames(sinkhorn_info) = c(row_info, "min", "max", "avg", "var", "argmin")
+  colnames(sinkhorn_info) = c(1:length(sinkhorn_info))
+  
+  addWorksheet(wb, sheetName = paste("Sheet", i, sep=""))
+  writeData(wb, sheet = paste("Sheet", i, sep=""), x=sinkhorn_info, rowNames = TRUE)
+  
+  distance_matrix[i, ] = apply(sinkhorn_info[1:8, ], 2, min)
+  min_angle_matrix[i, ] = row_info[apply(sinkhorn_info[1:8, ], 2, which.min)]
+  
+  cat("\n")
+  i=i+1
+}
+
+addWorksheet(wb, sheetName = "Distance matrix")
+writeData(wb, sheet = "Distance matrix", x=distance_matrix, rowNames = FALSE, colNames = FALSE)
+
+addWorksheet(wb, sheetName = "Min angle matrix")
+writeData(wb, sheet = "Min angle matrix", x=min_angle_matrix, rowNames = FALSE, colNames = FALSE)
+
+openxlsx::saveWorkbook(wb, file =paste(plot_dest_path, version, "_", analysis_type, scaling, "_", lambda,"_all_result.xlsx", sep=""), overwrite = TRUE)
+  
+  
+#### creating workbook to save the min angle matrix separately
+wb2 = openxlsx::createWorkbook()
+
+addWorksheet(wb2, sheetName = "Min angle matrix")
+writeData(wb2, sheet = "Min angle matrix", x=min_angle_matrix, rowNames = FALSE, colNames = FALSE)
+
+openxlsx::saveWorkbook(wb2, file =paste(parent, "Data/Supporting Files/Angle of Rotation/Angle_of_rotation_T4transport_", version, "_", scaling, "_", lambda,".xlsx", sep=""), 
+                       overwrite = TRUE)
+
+
+
+
