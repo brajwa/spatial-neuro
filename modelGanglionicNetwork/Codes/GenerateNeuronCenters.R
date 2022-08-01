59fba98ed4050b4a67a6361e796185da3a260493
Abida Sanjana Shemonti <ashemont@purdue.edu>
2022-08-01 18:35:05 -0400

    Update GenerateNeuronCenters.R

--- modelGanglionicNetwork/Codes/GenerateNeuronCenters.R
+++ modelGanglionicNetwork/Codes/GenerateNeuronCenters.R
@@ -8,7 +8,7 @@ for(lib in install_lib) install.packages(lib, dependencies=TRUE)
 sapply(load_lib, require, character=TRUE)
 
 
-##################################################################################################
+####################################
 generateNeuronCenters <- function(){
     #### extracting parent directory information for accessing input and output location
     dir = this.dir()
@@ -30,27 +30,25 @@ generateNeuronCenters <- function(){
     output_folder_path = paste(parent, "Outputs/", image_name, "/", sep="")
     if (!dir.exists(output_folder_path)) {dir.create(output_folder_path, recursive=TRUE)}
     
-    path_to_intensity_image = "D:\\Summer 2019\\R codes\\Research1.0InterganglionicNetwork2021\\Inputs\\Intensity Profile\\"
-    image_name = "Thick_mask_simulation_3.jpg"
-    
+    #### the spatial model parameters to generate neuron centers
     b_val = 0.003
     h_val = 0.01
-    
     g_val =0.78
     r_val = 0.0285
     
-    # intensity_profile = cimg2im(load.image(paste(image_path, image_name, sep="")), W=simulated.ppp$window)
-    intensity_profile = cimg2im(load.image(paste(path_to_intensity_image, image_name, sep="")))
+    intensity_profile = cimg2im(load.image(path_to_intensity_image))
     
-    #simulated_neuron_1 = rHardcore(beta = b_val, R = h_val, W=owin(intensity_profile$xrange, intensity_profile$yrange))
-    simulated_neuron_1 = rStraussHard(beta = b_val, gamma = g_val, R = r_val, H = h_val, W=owin(intensity_profile$xrange, intensity_profile$yrange))
+    #### simulate neuron centers with hardcore-Strauss process
+    simulated_neuron_1 = rStraussHard(beta = b_val, gamma = g_val, R = r_val, H = h_val, 
+                                      W=owin(intensity_profile$xrange, intensity_profile$yrange))
     
     plot(simulated_neuron_1, bg='magenta', pch=21, cex=0.9)
     
+    #### thin the simulated neuron centers based on the loaded intensity profile
     simulated_neuron = rthin(X=simulated_neuron_1, P=intensity_profile)
-    
     plot(simulated_neuron, bg='magenta', pch=21, cex=0.9)
     
+    #### adjust the window of the simulated neurons
     simulated_neuron$y = simulated_neuron$y - simulated_neuron$window$yrange[2]
     simulated_neuron$x = simulated_neuron$x - simulated_neuron$window$xrange[1]
     simulated_neuron$window$xrange = simulated_neuron$window$xrange- simulated_neuron$window$xrange[1]
@@ -61,31 +59,11 @@ generateNeuronCenters <- function(){
     simulated_neuron$window$xrange = c(0, abs(ganglia_ppp$window$xrange[2]))
     simulated_neuron$window$yrange = c(ganglia_ppp$window$yrange[1], 0)
     
-    #simulated_neuron$y = simulated_neuron$y - 3735
+    #### creating a directory to save the simulated neuron related files
+    sim_neuron_path = paste(output_folder_path, "Simulated Neuron/", sep="")
+    if (!dir.exists(sim_neuron_path)) {dir.create(sim_neuron_path, recursive=TRUE)}
     
-    plot(g2_lin, lwd=2)
-    plot(simulated_neuron, bg='magenta', pch=21, cex=0.6, add=T)
-    plot(ganglia_ppp, bg='black', pch=21, cex=0.6, add=T)
-    
-    write.csv(data.frame(x=simulated_neuron$x, y=simulated_neuron$y), 
-            "D://Summer 2019//R codes//Research1.0InterganglionicNetwork2021//Outputs//Simulated thingys//neuron_coord_new.csv", 
-            row.names = F)
-    
-    doc = read_pptx()
-    
-    doc = add_slide(doc, "Blank", "Office Theme")
-    doc = ph_with(doc, dml(code = plot(simulated_neuron, bg='magenta', pch=21, cex=0.8)), location = ph_location_fullsize())
-    
-    doc = add_slide(doc, "Blank", "Office Theme")
-    doc = ph_with(doc, dml(code = ({plot(g2_lin)
-    plot(ganglia_ppp, bg='black', pch=21, cex=0.9, add=T)})), location = ph_location_fullsize())
-    
-    doc = add_slide(doc, "Blank", "Office Theme")
-    doc = ph_with(doc, dml(code = ({plot(g2_lin, lwd=2)
-    plot(ganglia_ppp, bg='magenta', pch=21, cex=0.9, add=T)
-    plot(simulated_neuron, bg='magenta', pch=21, cex=0.9, add=T)})), location = ph_location_fullsize())
-    
-    
-    print(doc, target = paste(path_to_intensity_image, "Neuron_Simulated_new.pptx", sep=""))
-
+    write.csv(data.frame(x=simulated_neuron$x, y=simulated_neuron$y),
+              paste(sim_neuron_path, image_name, "_Simulated_Neuron_coord.csv", sep=""),
+              row.names = F)
 }
