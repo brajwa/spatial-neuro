76311d90ba9e3223f9c7892de5d90895ec4aa4fc
Abida Sanjana Shemonti <ashemont@purdue.edu>
2024-02-28 16:01:15 -0500

    Update LfunctAnova2.R

--- hybridSpatialModel/Code/LfunctAnova2.R
+++ hybridSpatialModel/Code/LfunctAnova2.R
@@ -1,9 +1,5 @@
 #### loading required libraries (there might be more libraries loaded than required)
-load.lib = c("deldir", "spatstat", "spatstat.utils", "magrittr", "dplyr", "igraph", "scales", "httr", "tidyverse", "ggnetwork", "ggplot2", "poweRlaw",
-             "imager", "viridis", "plotrix", "openxlsx", "tidyr", "spdep", "maptools", "tmap", "OpenImageR", "dismo", "lctools",
-             "officer", "rvg", "oce", "OneR", "RandomFieldsUtils", "RandomFields", "Cairo", "knitr", "scorepeak", "Rcpp", "emdist",
-             "RImageJROI", "svglite", "transport", "Barycenter", "T4transport", "wvtool", "adimpro", "reshape2", "proxy", "RColorBrewer", "tictoc",
-             "ggrepel", "scatterplot3d", "car", "e1071", "rgl", "this.path")
+load.lib = c("this.path", "spatstat")
 
 install.lib = load.lib[!load.lib %in% installed.packages()]
 for(lib in install.lib) install.packages(lib, dependencies=TRUE)
@@ -18,14 +14,14 @@ parent = strsplit(dir, folder)
 
 
 #### input location
-folder_path = paste(parent, "Data/Inputs/", sep = "")
+folder_path = paste(parent, "Data/Peripheral Axon/Inputs/", sep = "")
 data_files = list.files(path=folder_path, pattern = ".csv", full.names = FALSE)
 extension = ".csv"
 
 
 #### removing the sample with quite different L-function behavior for r-value computation
 #### sample: 11328_5_AB-2V_2_montage_pred
-data_files = data_files[data_files != "11328_5_AB-2V_2_montage_pred_mi.csv"]
+#data_files = data_files[data_files != "11328_5_AB-2V_2_montage_pred_mi.csv"]
 
 
 pp_list = list()
@@ -42,8 +38,7 @@ for (file_name in data_files) {
   
   axon_pp = ppp(x=axon_locations$X, y=axon_locations$Y, checkdup=F, window = retrieved_contour)
   
-  #axon_pp = rescale.ppp(axon_pp, s=axon_pp$window$yrange[2])
-  axon_pp = rescale.ppp(axon_pp, s=320153.4)  #pre-computed maximum y-range of all the fascicles
+  axon_pp = rescale.ppp(axon_pp, s=320153.4)  #pre-computed maximum y-range of all the fascicles (ScalingFactorComputation.R)
   axon_pp = shift.ppp(axon_pp, origin = "centroid")
   
   pp_list[[length(pp_list) + 1]] = axon_pp
@@ -60,12 +55,13 @@ r_vect_size = 100
 inhom_l_r_vect = seq(0, min(max_inhom_l_r), by=(min(max_inhom_l_r)/r_vect_size))
 
 
-inhom_l_all = data.frame(sample=data_files, group=c(rep(1, 17), rep(2, 11))) #1=vagus,2=pelvic
+inhom_l_all = data.frame(sample=data_files, group=c(rep(1, 18), rep(2, 11))) #1=vagus,2=pelvic
 
 
 #### computing inhomogeneous L function for the common range of r values (just computed above) for all the samples
 #### to figure out the most varying r. This r will be used in local computations.
 for(i in c(1:length(pp_list))){
+  cat(i, "\n")
   axon_pp = pp_list[[i]]
   inhom_l = Linhom(axon_pp, r=inhom_l_r_vect, correction = "border")
   
